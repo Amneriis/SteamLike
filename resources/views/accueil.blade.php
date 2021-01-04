de600409065cc6a7dcec2134a63de3041c617c43
Amneriis <46808792+Amneriis@users.noreply.github.com>
2021-01-04 14:20:39 +0100

    ajout vue admin & lien

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>SteamLike</title>
+
+    </head>
+    <body>
+    <a href="/admin">Panel admin</a>
+    </body>
+</html>
---
+++ resources/views/admin.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>SteamLike</title>
+
+    </head>
+    <body>
+        <h1>Panel Admin</h1>
+    <a href="/">Accueil</a>
+    </body>
+</html>
