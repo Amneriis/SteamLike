a299b8a6cdb6427bd5117200b2277d2a8a8b299e
Amneriis <46808792+Amneriis@users.noreply.github.com>
2021-01-27 14:01:27 +0100

    modifs css page de payement

--- resources/views/pages/payement.blade.php
+++ resources/views/pages/payement.blade.php
@@ -6,34 +6,6 @@
 
 @section('content')
     <div class="w-8/12">
-<<<<<<< HEAD
-        <div class="shadow-2xl">
-            <a>NOM : {{$jeux->nom_jeu}} || PRIX : {{$jeux->prix}}€</a>
-            <form action="" method="get">
-                <div>
-                    <label for="">Numéro de carte :</label>
-                    <input type="text" name="numCarte" placeholder="Numéro de carte" class="rounded-lg" required>
-                </div>
-                <div>
-                    <label for="">Titulaire de la carte :</label>
-                    <input type="text" name="proprioCarte" placeholder="Numéro Titulaire de la carte carte" class="rounded-lg" required>
-                </div>
-                <div>
-                    <label for="">Date d'expiration :</label>
-                    <input type="text" name="expiration" placeholder="Date d'expiration" class="rounded-lg" required>
-                </div>
-                <div>
-                    <label for="">Cryptogramme :</label>
-                    <input type="text" name="cryptogramme" placeholder="Cryptogramme" class="rounded-lg" required>
-                </div>
-                <div>
-                    <a class="bg-gray-800 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none" href="{{ route('jeu.pay',['id'=>$jeux->id]) }}">Acheter</a>
-                </div>
-            </form>
-        </div>
-    </div>
-        
-=======
         <a>NOM : {{$jeux->nom_jeu}} || PRIX : {{$jeux->prix}}€</a>
     </div>
     <form action={{ route('transactions.store') }} method="post">
@@ -42,23 +14,22 @@
     <input type="hidden" name="id_user" value="{{Auth::id()}}">
         <div>
             <label for="">Numéro de carte :</label>
-            <input type="text" name="numCarte" placeholder="Numéro de carte" required>
+            <input type="text" name="numCarte" placeholder="Numéro de carte" required class="rounded-lg">
         </div>
         <div>
             <label for="">Titulaire de la carte :</label>
-            <input type="text" name="proprioCarte" placeholder="Numéro Titulaire de la carte carte" required>
+            <input type="text" name="proprioCarte" placeholder="Numéro Titulaire de la carte carte" required class="rounded-lg">
         </div>
         <div>
             <label for="">Date d'expiration :</label>
-            <input type="text" name="expiration" placeholder="Date d'expiration" required>
+            <input type="text" name="expiration" placeholder="Date d'expiration" required class="rounded-lg">
         </div>
         <div>
             <label for="">Cryptogramme :</label>
-            <input type="text" name="cryptogramme" placeholder="Cryptogramme" required>
+            <input type="text" name="cryptogramme" placeholder="Cryptogramme" required class="rounded-lg">
         </div>
         <div>
             <button type="submit" class="bg-gray-800 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Acheter</button>
         </div>
     </form>
->>>>>>> 63c7b514cfe03bf13487620bfb17495d7d158bf8
 @endsection
