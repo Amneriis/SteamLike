a691e7728b676c686e99fc1b20283c2b7863ba48
Amneriis <46808792+Amneriis@users.noreply.github.com>
2021-01-27 10:22:09 +0100

    ajout form payement

--- resources/views/pages/payement.blade.php
+++ resources/views/pages/payement.blade.php
@@ -5,5 +5,22 @@
 @endsection
 
 @section('payement')
-    
+    <form action="" method="get">
+        <div>
+            <label for="">Numéro de carte</label>
+            <input type="text" name="numCarte" placeholder="Numéro de carte" required>
+        </div>
+        <div>
+            <label for="">Titulaire de la carte</label>
+            <input type="text" name="proprioCarte" placeholder="Numéro Titulaire de la carte carte" required>
+        </div>
+        <div>
+            <label for="">Date d'expiration</label>
+            <input type="text" name="expiration" placeholder="Date d'expiration" required>
+        </div>
+        <div>
+            <label for="">Cryptogramme</label>
+            <input type="text" name="cryptogramme" placeholder="Cryptogramme" required>
+        </div>
+    </form>
 @endsection
