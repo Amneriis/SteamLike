84fcf38a192da7e5bb58f14cc8807e7c28949039
Amneriis <46808792+Amneriis@users.noreply.github.com>
2021-01-27 11:17:16 +0100

    Ajout bouton payement

--- resources/views/pages/payement.blade.php
+++ resources/views/pages/payement.blade.php
@@ -10,20 +10,23 @@
     </div>
     <form action="" method="get">
         <div>
-            <label for="">Numéro de carte</label>
+            <label for="">Numéro de carte :</label>
             <input type="text" name="numCarte" placeholder="Numéro de carte" required>
         </div>
         <div>
-            <label for="">Titulaire de la carte</label>
+            <label for="">Titulaire de la carte :</label>
             <input type="text" name="proprioCarte" placeholder="Numéro Titulaire de la carte carte" required>
         </div>
         <div>
-            <label for="">Date d'expiration</label>
+            <label for="">Date d'expiration :</label>
             <input type="text" name="expiration" placeholder="Date d'expiration" required>
         </div>
         <div>
-            <label for="">Cryptogramme</label>
+            <label for="">Cryptogramme :</label>
             <input type="text" name="cryptogramme" placeholder="Cryptogramme" required>
+        </div>
+        <div>
+            <a class="bg-gray-800 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none" href="{{ route('jeu.pay',['id'=>$jeux->id]) }}">Acheter</a>
         </div>
     </form>
 @endsection
