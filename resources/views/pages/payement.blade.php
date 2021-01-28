61cba9a1efe6a8c14b13a9bf777421c8d2cb0a1c
Amneriis <46808792+Amneriis@users.noreply.github.com>
2021-01-28 10:46:56 +0100

    add css payement

--- resources/views/pages/payement.blade.php
+++ resources/views/pages/payement.blade.php
@@ -9,31 +9,34 @@
 @endsection
 
 @section('content')
-    <div class="w-8/12">
+    <div class="w-6/12">
         <a>NOM : {{$jeux->nom_jeu}} || PRIX : {{$jeux->prix}}€</a>
+    
+        <div class="w-full bg-grey-100 rounded shadow-lg p-8 m-4 flex items-center">
+            <form action={{ route('transactions.store') }} method="post" class="mb-4 w-1/2 mx-auto">
+            @csrf
+            @method('POST')
+            <input type="hidden" name="id_jeu" value="{{$jeux->id}}">
+                <div class="my-4">
+                    <label for="">Numéro de carte :</label>
+                    <input type="text" name="numCarte" placeholder="Numéro de carte" required class="bg-gray-100 p-2 rounded border-2">
+                </div>
+                <div class="my-4">
+                    <label for="mb-2 uppercase font-bold text-lg text-gray-600 w-full">Titulaire de la carte :</label>
+                    <input type="text" name="proprioCarte" placeholder="Numéro Titulaire de la carte carte" required class="bg-gray-100 p-2 rounded border-2">
+                </div>
+                <div class="my-4">
+                    <label for="mb-2 uppercase font-bold text-lg text-gray-600 w-full">Date d'expiration :</label>
+                    <input type="text" name="expiration" placeholder="Date d'expiration" required class="bg-gray-100 p-2 rounded border-2">
+                </div>
+                <div class="my-4">
+                    <label for="mb-2 uppercase font-bold text-lg text-gray-600 w-full">Cryptogramme :</label>
+                    <input type="text" name="cryptogramme" placeholder="Cryptogramme" required class="bg-gray-100 p-2 rounded border-2">
+                </div>
+                <div class="my-4">
+                    <button type="submit" class="block bg-blue-700 hover:bg-blue-500 text-white uppercase text-lg p-4 rounded">Acheter</button>
+                </div>
+            </form>
+        </div>
     </div>
-    <form action={{ route('transactions.store') }} method="post">
-    @csrf
-    @method('POST')
-    <input type="hidden" name="id_jeu" value="{{$jeux->id}}">
-        <div>
-            <label for="">Numéro de carte :</label>
-            <input type="text" name="numCarte" placeholder="Numéro de carte" required class="rounded-lg">
-        </div>
-        <div>
-            <label for="">Titulaire de la carte :</label>
-            <input type="text" name="proprioCarte" placeholder="Numéro Titulaire de la carte carte" required class="rounded-lg">
-        </div>
-        <div>
-            <label for="">Date d'expiration :</label>
-            <input type="text" name="expiration" placeholder="Date d'expiration" required class="rounded-lg">
-        </div>
-        <div>
-            <label for="">Cryptogramme :</label>
-            <input type="text" name="cryptogramme" placeholder="Cryptogramme" required class="rounded-lg">
-        </div>
-        <div>
-            <button type="submit" class="bg-gray-800 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Acheter</button>
-        </div>
-    </form>
 @endsection
