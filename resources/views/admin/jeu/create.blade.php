92aec5dbf4fb03c3b91f41bff0a02da90000004e
Amneriis <46808792+Amneriis@users.noreply.github.com>
2021-01-28 10:26:02 +0100

    Modif form création

--- resources/views/admin/jeu/create.blade.php
+++ resources/views/admin/jeu/create.blade.php
@@ -5,26 +5,27 @@
 @endsection
 
 @section('jeu')
-<div class="flex items-center bg-teal-lighter">
-    <div class="w-full bg-grey-100 rounded shadow-lg p-8 m-4">
-        <h3 class="mb-8">Créer un jeu</h3>
-        <form action={{ route('jeu.store') }} class="mb-4 md:flex md:flex-wrap md:justify-between" method="post">
+<div class="bg-teal-lighter">
+    <h3 class="">Créer un jeu</h3>
+    <div class="w-full bg-grey-100 rounded shadow-lg p-8 m-4 flex items-center">
+        
+        <form action={{ route('jeu.store') }} class="mb-4 w-1/2 mx-auto" method="post">
             @csrf
             @method('POST')
-            <div class="mb-4 md:flex md:flex-wrap md:justify-between">
-                <label class="mb-2 uppercase font-bold text-lg text-gray-600" for="nom_jeu">Nom du Jeu :</label>
+            <div class="my-4">
+                <label class="mb-2 uppercase font-bold text-lg text-gray-600 w-full" for="nom_jeu">Nom du Jeu :</label>
                 <input type="text" id="nom-form" name="nom_jeu" class="bg-gray-100 p-2 rounded border-2" placeholder="Nom du jeu" required>
             </div>
-            <div class="field-group mb-4 md:w-1/2 m-0 m-auto">
+            <div class="my-4">
                 <label class="mb-2 uppercase font-bold text-lg text-gray-600" for="prix">Prix du Jeu :</label>
                 <input type="text" id="prix-form" name="prix" class="bg-gray-100 p-2 rounded border-2" placeholder="Prix du jeu" required>
             </div>
-            <div class="field-group mb-4 md:w-1/2 m-0 m-auto">
+            <div class="my-4">
                 <label class="mb-2 uppercase font-bold text-lg text-gray-600" for="prix">URL Image :</label>
                 <input type="text" id="url-form" name="url" class="bg-gray-100 p-2 rounded border-2" placeholder="URL Image">
             </div>
 
-            <button class="block bg-blue-700 hover:bg-blue-500 text-white uppercase text-lg mx-auto p-4 rounded" type="submit">Ajouter</button>
+            <button class="block bg-blue-700 hover:bg-blue-500 text-white uppercase text-lg p-4 rounded" type="submit">Ajouter</button>
         </form>
     </div>
 </div>
